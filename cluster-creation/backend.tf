4eeef218e805bd84f6920f1ba74409c06358d803
anilsree6 <149375170+anilsree6@users.noreply.github.com>
2023-11-04 22:16:43 +0530

    Create backend.tf

---
+++ cluster-creation/backend.tf
@@ -0,0 +1,10 @@
+/*terraform {
+  backend "s3" {
+    bucket = "terraform-st-backend"
+    key = "testing/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    dynamodb_table = "terraform_state"
+  }
+}*/
+
