c7c9d6d271b352d6310515dd7901eb354df14ae6
anilsree6 <149375170+anilsree6@users.noreply.github.com>
2023-11-05 21:47:41 +0530

    Update backend.tf

--- cluster-creation/backend.tf
+++ cluster-creation/backend.tf
@@ -2,7 +2,7 @@
   backend "s3" {
     bucket = "terraform-st-backend"
     key = "testing/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
     encrypt = true
     dynamodb_table = "terraform_state"
   }
